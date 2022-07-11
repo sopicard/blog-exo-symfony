cd333bd02f78b19b53b91f36cd0ec7ab49a4a412
sopicard <sogautrelet@gmail.com>
2022-07-11 16:24:18 +0200

    add Submit btn on forms

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,14 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            //génère l'input pour le title avec valeur par défaut.
+            //Si je souhaite par ex un text area, il faudra que je le rajoute moi-même
             ->add('title')
             ->add('color')
             ->add('description')
             ->add('isPublished')
+            // je peux en rajouter
+            ->add("submit", SubmitType::class)
         ;
     }
 
